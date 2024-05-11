fe2ea7033007c310937f289cf9069c3efd1741c9
0x7d8 <rjansengd@gmail.com>
2024-05-12 00:54:03 +0200

    allow setting port

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ const write = !options.nosave ? fs.createWriteStream('../history.raw', {
 }) : null
 
 const server = new Server(Runtime, {
-  port: 8000
+  port: process.env.PORT ? parseInt(process.env.PORT) : 8000
 }, [], {
   requests: 0
 })
